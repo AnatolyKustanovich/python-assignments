8f27f7e7fc346dc6170064b68073c661dbb71546
Anatoly <53263438+AnatolyKustanovich@users.noreply.github.com>
2024-11-12 22:03:59 +0200

    Update Rectangle.py

--- Day02/Rectangle.py
+++ Day02/Rectangle.py
@@ -1,6 +1,5 @@
-Python 3.12.1 (tags/v3.12.1:2305ca5, Dec  7 2023, 22:03:25) [MSC v.1937 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> width = 10
-... length = 20
-... area = width * length
-... circumference = 2*(width+length)
+width = 10
+length = 20
+area = width * length
+circumference = 2*(width+length)
+print(area, circumference)
