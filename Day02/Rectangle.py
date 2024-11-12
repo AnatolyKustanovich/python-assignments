71f2bd6ccabc6dc66277a47d732fb5639d3357f5
Anatoly <53263438+AnatolyKustanovich@users.noreply.github.com>
2024-11-12 22:05:17 +0200

    Update Rectangle.py

--- Day02/Rectangle.py
+++ Day02/Rectangle.py
@@ -2,4 +2,4 @@ width = 10
 length = 20
 area = width * length
 circumference = 2*(width+length)
-print(area, circumference)
+print(" Area of rectangle is:", area, '\n', "Circumference of rectangle is:", circumference) 
