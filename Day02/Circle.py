ecd97a337c8be5486b3efa1972a2c4641c77e45d
Anatoly <amkustanovich@gmail.com>
2024-11-19 11:44:06 +0200

    modified:   Day02/Circle.py

--- Day02/Circle.py
+++ Day02/Circle.py
@@ -2,4 +2,4 @@ radius = 10
 pi = 3.14
 area = pi*radius*radius
 circumference = 2*pi*radius
-print(" Area of circle is:", area, '\n', "Circumference of circle is:", circumference) 
+print(" Area of circle is:", area, '\n', "Circumference of circle is:", circumference)
