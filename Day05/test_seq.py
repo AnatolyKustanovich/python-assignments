16f7676175609e11900870db5495a5b04aa19765
Anatoly <amkustanovich@gmail.com>
2024-12-10 00:08:30 +0200

    test_seq

--- Day05/test_seq.py
+++ Day05/test_seq.py
@@ -4,14 +4,10 @@ from seq import calculate_statistics
 
 def test_a_seq():
     seq = "ACCTGXXCXXGTTACTGGGCXTTGTXX"
-    stats, percentages, total = calculate_statistics(seq)
-    assert stats == {"A": 2, "C": 5, "G": 6, "T": 7, "Unknown": 7}
-    assert percentages == {"A": 2, "C": 5, "G": 6, "T": 7, "Unknown": 7}
-    assert total == 27
+    stats = calculate_statistics(seq)  # Only one return value
+    assert stats == {"A": 2, "C": 5, "G": 6, "T": 7, "Unknown": 7, "Total": 27}
 
 def test_b_seq():
     seq = "ACCGGGTTTT"
-    stats, percentages, total = calculate_statistics(seq)
-    assert stats == {"A": 1, "C": 2, "G": 3, "T": 4, "Unknown": 0}
-    assert percentages == {"A": 2, "C": 5, "G": 6, "T": 7, "Unknown": 7}
-    assert total == 10
+    stats = calculate_statistics(seq)  # Only one return value
+    assert stats == {"A": 1, "C": 2, "G": 3, "T": 4, "Unknown": 0, "Total": 10}
